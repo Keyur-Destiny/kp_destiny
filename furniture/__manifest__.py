9a97e24634f0990604e940a5437da6c7cd75cdcf
Keyur-Destiny <keyur.patel@thedestinysolutions.com>
2023-10-26 14:42:39 +0530

    furniture modules added

---
+++ furniture/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Furniture',
+    'version': '1.0',
+    'summary': 'Furniture snippet',
+    'author': 'Ashish',
+    'website': 'https://www.oddo.com',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/website_menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+
+}
