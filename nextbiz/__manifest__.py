72caafd4a1901a60a0d7a15d04c6736419e25e66
keyur <keyur.patel@thedestinysolutions.com>
2023-12-05 16:06:34 +0530

    nextbiz module added

---
+++ nextbiz/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Nextbiz',
+    'version': '1.0',
+    'summary': 'nextbiz hover',
+    'author': 'Ashish',
+    'website': 'https://www.oddo.com',
+    'depends': ['website'],
+#     'data': [
+#         'views/website_template.xml',
+#     ],
+    'assets': {
+        'web.assets_frontend': [
+            'nextbiz/static/src/img/consultpage1_feature1.jpg',
+        ],
+    },
+    'installable': True,
+    'application': True,
+
+}
